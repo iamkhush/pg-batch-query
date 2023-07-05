b6679196f6cc3ea1162821714994cb89c57f4b54
iamkhush <ankush.chadda@gmail.com>
2023-07-05 23:36:55 +0200

    Moving code to its own repo

---
+++ bench.ts
@@ -0,0 +1,100 @@
+import pg from 'pg'
+import BatchQuery from './src'
+
+const insert = (value) => ({
+  text: 'INSERT INTO foobar(name, age) VALUES ($1, $2)',
+  values: ['joe' + value, value],
+})
+
+const select = (value) => ({
+  text: 'SELECT FROM foobar where name = $1 and age = $2',
+  values: ['joe' + value, value],
+})
+
+let counter = 0
+
+const simpleExec = async (client, getQuery, count) => {
+  const query = getQuery(count)
+  await client.query({
+    text: query.text,
+    values: query.values,
+    rowMode: 'array',
+  })
+}
+
+const batchExec = async (client, getQuery, count, queryCount = 5) => {
+  const query = getQuery(count)
+  const values: any[][] = []
+  for (let i = 0; i < queryCount; i++) {
+    values.push(['joey' + i, i])
+  }
+
+  const batchQuery = new BatchQuery({
+    name: 'optional'+ counter++,
+    text: query.text,
+    values
+  })
+  await client.query(batchQuery).execute()
+}
+
+const bench = async (client, mainMethod, q, time, queryCount = 5) => {
+  let start = Date.now()
+  let count = 0
+  while (true) {
+    await mainMethod(client, q, count, queryCount = 5)
+    count++
+    if (Date.now() - start > time) {
+      return count
+    }
+  }
+}
+
+const run = async () => {
+  const client = new pg.Client()
+  await client.connect()
+  console.log('start')
+  await client.query('CREATE TEMP TABLE foobar(name TEXT, age NUMERIC)')
+  console.log('warmup done')
+  const seconds = 5
+
+  // run benchmark tests
+  for (let i = 0; i < 4; i++) {
+    let queries = await bench(client, simpleExec, insert, seconds * 1000)
+    console.log('')
+    console.log('insert queries:', queries)
+    console.log('qps', queries / seconds)
+    console.log('on my laptop best so far seen 16115.4 qps')
+
+    queries = await bench(client, batchExec, insert, seconds * 1000)
+    console.log('')
+    console.log('insert batch queries:', queries * 5)
+    console.log('qps', queries * 5 / seconds)
+    console.log('on my laptop best so far seen 42646 qps')
+
+    queries = await bench(client, simpleExec, select, seconds * 1000)
+    console.log('')
+    console.log('select queries:', queries)
+    console.log('qps', queries / seconds)
+    console.log('on my laptop best so far seen 18579.8 qps')
+
+    queries = await bench(client, batchExec, select, seconds * 1000)
+    console.log('')
+    console.log('select batch queries:', queries * 5)
+    console.log('qps', queries * 5 / seconds)
+    console.log('on my laptop best so far seen 44887 qps')
+  }
+
+  // find ideal 
+  // for (let i = 500; i < 501; i++) {
+  //   let queries = await bench(client, batchExec, select, seconds * 1000, i)
+  //   console.log('')
+  //   console.log('insert batch queries:', queries * i)
+  //   console.log('qps for ' + i + 'queries', queries * i / seconds)
+  //   // console.log('on my laptop best so far seen 42646 qps')
+  // }
+
+  await client.end()
+  await client.end()
+}
+
+run().catch((e) => Boolean(console.error(e)) || process.exit(-1))
---
+++ src/index.ts
@@ -0,0 +1,129 @@
+import { Submittable, Connection, QueryResult, QueryResultRow } from 'pg'
+const Result = require('pg/lib/result.js')
+const utils = require('pg/lib/utils.js')
+let nextUniqueID = 1 // concept borrowed from org.postgresql.core.v3.QueryExecutorImpl
+
+interface BatchQueryConfig {
+  name?: string
+  text: string
+  values: string[][]
+}
+
+class BatchQuery<T extends QueryResultRow> implements Submittable {
+  private name: string | null
+  private text: string
+  private values: string[][]
+  private connection: Connection | null
+  private _portal: string | null
+  private _result: typeof Result | null
+  private _results: typeof Result[]
+  private callback: null | ((err: Error | null, rows: QueryResult<T>[] | null) => void)
+
+  constructor(batchQuery: BatchQueryConfig) {
+    const { name, values, text } = batchQuery
+
+    this.name = name ?? null
+    this.values = values
+    this.text = text
+    this.connection = null
+    this._portal = null
+    this._result = new Result()
+    this._results = []
+    this.callback = null
+
+    for (const _ of values) {
+      if (!Array.isArray(values)) {
+        throw new Error(
+          'Batch commands require each set of values to be an array. e.g. values: any[][]'
+        )
+      }
+    }
+  }
+
+  public submit(connection: Connection): void {
+    this.connection = connection
+
+    // creates a named prepared statement
+    connection.parse(
+      {
+        text: this.text,
+        name: this.name ?? '',
+        types: [],
+      },
+      true
+    )
+
+    this.values.map((val) => {
+      this._portal = 'C_' + nextUniqueID++
+      connection.bind(
+        {
+          statement: this.name ?? '',
+          values: val,
+          portal: this._portal,
+          valueMapper: utils.prepareValue,
+        },
+        true
+      )
+
+      // maybe we could avoid this for non-select queries
+      connection.describe(
+        {
+          type: 'P',
+          name: this._portal,
+        },
+        true
+      )
+
+      connection.execute({ portal: this._portal }, true)
+    })
+
+    this.connection.sync()
+  }
+
+  public execute(): Promise<QueryResult<T>[]> {
+    return new Promise((resolve, reject) => {
+      this.callback = (err: Error | null, rows: QueryResult<T>[] | null = null) =>
+        err ? reject(err) : rows ? resolve(rows) : []
+    })
+  }
+
+  private handleError(err: Error) {
+    this.connection?.flush()
+    if (this.callback) {
+      this.callback(err, null)
+    }
+  }
+
+  private handleReadyForQuery() {
+    if (this.callback) {
+      // eslint-disable-next-line no-useless-catch
+      try {
+        this.callback(null, this._results)
+      } catch (err) {
+        throw err
+      }
+    }
+  }
+
+  private handleRowDescription(msg: any) {
+    this._result?.addFields(msg.fields)
+  }
+
+  private handleDataRow(msg: any) {
+    const row = this._result.parseRow(msg.fields)
+    this._result.addRow(row)
+  }
+
+  private handleCommandComplete(msg: any) {
+    this._result.addCommandComplete(msg)
+    this._results.push(this._result)
+    this._result = new Result()
+    this.connection?.close({ type: 'P', name: this._portal ?? '' }, true)
+  }
+
+  private handleEmptyQuery() {
+    this.connection?.sync()
+  }
+}
+
+export = BatchQuery
